dc16770160877116595f3f01459868c5d1bc88cd
Pace Wasden <pacewasden@gmail.com>
2020-12-07 23:29:54 -0700

    Views are working correctly. Just have to set up the stories and then we are good to go

--- mysite/madlib/urls.py
+++ mysite/madlib/urls.py
@@ -6,5 +6,13 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('game1/', views.word_list, name='word_list'),
     path('gameview1.html/', views.gameview, name='gameview'),
+    path('game2/', views.word_list2, name='word_list2'),
+    path('gameview2.html/', views.gameview2, name='gameview2'),
+    path('game3/', views.word_list3, name='word_list3'),
+    path('gameview3.html/', views.gameview3, name='gameview3'),
+    path('game4/', views.word_list4, name='word_list4'),
+    path('gameview4.html/', views.gameview4, name='gameview4'),
+    path('game5/', views.word_list5, name='word_list5'),
+    path('gameview5.html/', views.gameview5, name='gameview5'),
     path('deletewords/', views.deletewords, name='deletewords')
 ]
--- mysite/madlib/views.py
+++ mysite/madlib/views.py
@@ -30,3 +30,65 @@ def deletewords(request):
         word_list.delete()
         return redirect('madlib:index')
     return render(request, 'madlib/deleteword.html',{'word_list':word_list})
+
+#This is view 2
+def word_list2(request):
+    form = WordForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('/gameview2.html')
+    return render(request, 'madlib/game2.html', {'form':form})
+
+def gameview2(request):
+    word_list = Words.objects.filter()[:1].get()
+    context = {
+        'word_list': word_list,
+    }
+    return render(request, 'madlib/gameview2.html', context)
+
+#This is view 3
+def word_list3(request):
+    form = WordForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('/gameview3.html')
+    return render(request, 'madlib/game3.html', {'form':form})
+
+def gameview3(request):
+    word_list = Words.objects.filter()[:1].get()
+    context = {
+        'word_list': word_list,
+    }
+    return render(request, 'madlib/gameview3.html', context)
+#This is view 4
+def word_list4(request):
+    form = WordForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('/gameview4.html')
+    return render(request, 'madlib/game4.html', {'form':form})
+
+def gameview4(request):
+    word_list = Words.objects.filter()[:1].get()
+    context = {
+        'word_list': word_list,
+    }
+    return render(request, 'madlib/gameview4.html', context)
+#This is view 5
+def word_list5(request):
+    form = WordForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('/gameview5.html')
+    return render(request, 'madlib/game5.html', {'form':form})
+
+def gameview5(request):
+    word_list = Words.objects.filter()[:1].get()
+    context = {
+        'word_list': word_list,
+    }
+    return render(request, 'madlib/gameview5.html', context)
